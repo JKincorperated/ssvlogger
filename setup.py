020790dbe2eb43f92d682dd620dec7b40522cae0
JKinc <73645805+JKincorperated@users.noreply.github.com>
2023-12-22 21:38:44 +0000

    Update workflows

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# pylint: skip-file
+
+from setuptools import setup, find_packages
+
+setup(
+    name='ssvlogger',
+    version='1.0',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'mycommand = ssvlogger.logger:main'
+        ]
+    },
+    # metadata for upload to PyPI
+    author='JKinc',
+    author_email='your.email@example.com',
+    description='A simple python package to parse SSV node logs and make them legible',
+    keywords='SSV logging',
+    url='https://github.com/JKincorperated/ssvlogger',  # project home page
+)
